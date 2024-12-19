77cf457cdb6d1fdaed9916d3ff854c7218f6b54a
Scythrine 05 | Rohan Murmu <scythrine05@gmail.com>
2024-12-20 02:32:37 +0530

    Binding port added

--- server/src/server.js
+++ server/src/server.js
@@ -6,6 +6,6 @@ const { PORT } = require("./utils/constants");
 const server = http.createServer(app);
 initSocket(server);
 
-server.listen(PORT, () => {
+server.listen(PORT, "0.0.0.0", () => {
   console.log(`Server running on port: ${PORT}`);
 });
