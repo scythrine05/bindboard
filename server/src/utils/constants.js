b8752c69b686795565989e3c08a94797426a3726
Scythrine 05 | Rohan Murmu <scythrine05@gmail.com>
2024-12-19 21:30:58 +0530

    Configured dotenv for environment variables

--- server/src/utils/constants.js
+++ server/src/utils/constants.js
@@ -1,4 +1,6 @@
+require("dotenv").config();
+
 module.exports = {
-    PORT: process.env.PORT || 5000,
-    EXPIRE_TIME: process.env.EXPIRY_TIME || 7200,
-  };
+  PORT: process.env.PORT || 5000,
+  EXPIRE_TIME: process.env.EXPIRY_TIME || 7200,
+};
